24607e06e60c516b8979a3b8ce35223012787310
Polina <pshibanova00@mail.ru>
2021-03-02 22:24:29 +0300

    feat: completed the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  if (!arr || arr.length == 0){
+    return 0;
+  }
+  const arrSorted = arr.sort((a, b) => a-b);
+  let arrMin = arrSorted[0];
+  return arrMin;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if (!arr || arr.length == 0){
+    return 0;
+  }
+  const arrSorted = arr.sort((a, b) => a-b);
+
+  const arrMax = arrSorted[arrSorted.length-1];
+  return arrMax;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if (!arr || arr.length == 0){
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  let arrAverage = sum / arr.length;
+   return arrAverage
 }
